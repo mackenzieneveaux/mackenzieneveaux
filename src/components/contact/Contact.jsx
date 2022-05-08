953b609d497c3675b231ef74786f3104b4428cea
Mackenzie Neveaux <someone@nowhere.com>
2022-05-07 19:01:02 -0500

    moblie view  test

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import "./contact.css"
- import pink from "../img/pink.png"
+import pink from "../img/pink.png"
 import phone from "../img/phone.png"
 import mail from "../img/mail.png"
 import address from "../img/address.png"
@@ -9,32 +9,6 @@ import React, { useContext, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { ThemeContext } from "../../context"
 
-
-
-// const Contact = () => {
-//     const formRef = useRef()
-//     const [done, setDone] = useState(false)
-//     const theme =  useContext(ThemeContext);
-//     const darkMode = theme.state.darkMode;
-
-
-//     const handleSubmit = (e)=>{
-//       e.perventDefault();
-//       emailjs
-//       .sendForm(
-//       "service_mackenzieneveaux", 
-//      "template_oemwkk9", 
-//       formRef.current, "AYUvEiUN1AF2yXq5M", 
-//       )
-//       .then((result) => {
-//           console.log(result.text);
-//           setDone(true)
-//       }, 
-//       (error) => {
-//           console.log(error.text);
-//       }
-//       );
-//   };
    
 export const Contact = () => {
   const formRef = useRef();
